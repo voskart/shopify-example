20cd96997f28713301fd26b88db57c917c53bb9b
ginetrix <voskobojnikov.artemij@gmail.com>
2015-09-07 16:18:49 +0200

    first table view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+    @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 10})
   end
 end
