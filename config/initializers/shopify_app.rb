4af2996e1fd942365c69c6805f2e6d3abd24586f
ginetrix <voskobojnikov.artemij@gmail.com>
2015-09-07 15:30:23 +0200

    fixed environment variables

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
-  config.api_key = "<api_key>"
-  config.secret = "<secret>"
+  config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
+  config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
   config.redirect_uri = "<redirect_uri>"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
