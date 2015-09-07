63a30b827bf7d6512f2e649766c13ea7d81cbce2
ginetrix <voskobojnikov.artemij@gmail.com>
2015-09-07 16:02:59 +0200

    added bootstrap

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
   config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
-  config.redirect_uri = "<redirect_uri>"
+  config.redirect_uri = "https://localhost:3000/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
